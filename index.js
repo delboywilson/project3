0bf9d8fe8e6862f880e611108e9d1e0040e944ed
delboywilson <75227946+delboywilson@users.noreply.github.com>
2021-02-11 14:33:39 +1000

    step 3 completed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// step 1 - set up
 const express = require('express')
 const db = require ('./data.js')
 const app = express()
 const PORT = 3000
 
+// step 2 
 app.get('/', (req, res) => {
   res.send('Welcome to our schedule website')
 })
@@ -11,16 +13,34 @@ app.get('/users', (req, res) => {
   res.json(db.users)
 })
 
-app.get('users/:0', (req, res) => {
-  // console.log(req.params)
-  res.send('0 is the user')
-})
-
 app.get('/schedules', (req, res) => {
   res.json(db.schedules)
 })
 
+// step 3a
 
-app.listen(PORT, () => {
-  console.log(`server is listening on localhost${PORT}`)
+app.get('/users/:singleUser', (req, res) => {
+  
+  const userId = req.params.singleUser
+
+  console.log(req.params)
+  res.json(db.users[userId])
+})
+
+// step 3b
+
+app.get('/users/:singleUser/schedules', (req, res) => {
+  
+  const userId = req.params.singleUser
+  const scheduleId = []
+
+  for (i=0; i < db.schedules.length; i ++) {
+    if (db.schedules[i]['user_id'] == userId)
+    scheduleId.push(db.schedules[i])
+  }
+  return res.send(scheduleId)
+})
+
+app.post('/users', (req, res) => {
+
 })
