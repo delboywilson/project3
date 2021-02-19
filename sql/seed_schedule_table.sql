186fd46b65df34352c334e0d18efa868a76b69b7
delboywilson <75227946+delboywilson@users.noreply.github.com>
2021-02-19 14:59:10 +1000

    Add TIMESTAMPTZ type into database

--- sql/seed_schedule_table.sql
+++ sql/seed_schedule_table.sql
@@ -1,7 +1,7 @@
-INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('James Bond', '1', '08:00', '14:00');
+INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('James Bond', '1', '2021-02-19 08:00', '2021-02-19 14:00');
 
-INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Tony Stark', '1', '12:00', '18:00');
+INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Tony Stark', '1', '2021-02-19 12:00', '2021-02-19 18:00');
 
-INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Ali G', '2', '10:00', '16:00');
+INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Ali G', '2', '2021-02-19 10:00', '2021-02-19 16:00');
 
-INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Donald Duck', '3', '13:00', '20:00');
+INSERT INTO schedule(user_name, day, start_time, end_time) VALUES ('Donald Duck', '3', '2021-02-19 13:00', '2021-02-19 20:00');
